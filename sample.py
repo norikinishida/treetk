b71bbdd729f692a09382fe3e28ecb38b33389109
norikinishida <norikinishida@gmail.com>
2019-01-30 16:29:49 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -183,7 +183,11 @@ sexp = treetk.preprocess("(S (NP (DT a) (NN boy)) (VP (VP (VBD saw) (NP (DT a) (
 ctree = treetk.sexp2tree(sexp, with_nonterminal_labels=True, with_terminal_labels=True)
 treetk.pretty_print(ctree)
 def func_head_rule(node):
-    # a simple example of the head-selection rules that specify a head subtree among the children nodes
+    """
+    :type node: NonTerminal
+    :rtype: int
+    """
+    # Please write rules that specify a head-subtree among the child nodes
     if node.label == "S" and node.children[0].label == "NP" and node.children[1].label == "VP":
         return 1 # the second child
     elif node.label == "NP" and node.children[0].label == "DT" and node.children[1].label == "NN":
@@ -197,7 +201,13 @@ def func_head_rule(node):
     else:
         return 0
 def func_label_rule(node, i, j):
-    # a simple example of the rules that specify an arc label given parent and head/dependent nodes
+    """
+    :type node: NonTerminal
+    :type i: int
+    :type j: int
+    :rtype: str
+    """
+    # Please write a function that specifies the relation label (string) between the head (i.e., node.children[i]) and the dependent (i.e., node.children[j]).
     return node.label
     # return "%s,%s,%s" % (node.label, node.children[i].label, node.children[j].label)
 dtree = treetk.ctree2dtree(ctree, func_head_rule, func_label_rule)
